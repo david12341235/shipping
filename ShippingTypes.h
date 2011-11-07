b9ceecc7d76d778ece799b0fd04a0a60871914bd
David Garcia <david.agg@gmail.com>
2011-11-07 10:20:42 -0800

    added NumPackages ordinal type and added string conversion for all types

--- ShippingTypes.h
+++ ShippingTypes.h
@@ -1,36 +1,50 @@
 #ifndef SHIPPINGTYPES
 #define SHIPPINGTYPES
 
+#include <sstream>
+
 namespace Shipping {
 
 class Mile : public Ordinal<Mile, unsigned int> {
 public:
 	Mile( unsigned int num ) : Ordinal<Mile, unsigned int>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 class Difficulty : public Ordinal<Difficulty, double> {
 public:
 	Difficulty( double num ) : Ordinal<Difficulty, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
+};
+
+class NumPackages : public Ordinal<NumPackages, double> {
+public:
+	NumPackages( double num ) : Ordinal<NumPackages, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 class Percentage : public Ordinal<Percentage, double> {
 public:
 	Percentage( double num ) : Ordinal<Percentage, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 class Mph : public Ordinal<Mph, double> {
 public:
 	Mph( double num ) : Ordinal<Mph, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 class Dollar : public Ordinal<Dollar, double> {
 public:
 	Dollar( double num ) : Ordinal<Dollar, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 class Hour : public Ordinal<Hour, double> {
 public:
 	Hour( double num ) : Ordinal<Hour, double>(num) {}
+	operator string() const { std::ostringstream oss; oss << value(); return oss.str(); };
 };
 
 } /* end namespace */
