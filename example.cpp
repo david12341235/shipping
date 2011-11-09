330e629ad0cb5446a4db663b977f3763b6ad38f3
bhh1988 <bhh1988@bhh1988-H67MA-USB3-B3.(none)>
2011-11-08 18:59:41 -0800

    commit before pull

--- example.cpp
+++ example.cpp
@@ -116,7 +116,7 @@ int main(int argc, char *argv[]) {
     }
 
     cout << "**** explore customer1 : distance 1500 ****" << endl;
-    cout << conn->attribute("explore customer1 : distance 1500") << endl;
+    cout << conn->attribute("explore customer1 : cost 400 distance 1500 expedited time 10") << endl;
     cout << endl;
 
     cout << "*** connect customer2 : customer1 ****" << endl;
