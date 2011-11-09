415c218dc89fe49e32acc748115f8f23f7a2c8df
David Garcia <david.agg@gmail.com>
2011-11-08 19:45:40 -0800

    added old example

--- example.cpp
+++ example.cpp
@@ -10,11 +10,127 @@ using std::cerr;
 using std::endl;
 using std::string;
 
+int example0();
 int example1();
 int example2();
 
 int main(int argc, char *argv[]) {
-	return example1();
+	return example0();
+}
+
+int example0() {
+    Ptr<Instance::Manager> manager = shippingInstanceManager();
+
+    if (!manager) {
+        cerr << "Unexpected NULL manager." << endl;
+        return 1;
+    }
+
+    Ptr<Instance> stats = manager->instanceNew("myStats", "Stats");
+
+    if (!stats) {
+        cerr << "Unexpected NULL stats." << endl;
+        return 1;
+    }
+
+    Ptr<Instance> fleet = manager->instanceNew("myFleet", "Fleet");
+
+    if (!fleet) {
+        cerr << "Unexpected NULL." << endl;
+        return 1;
+    }
+
+    fleet->attributeIs("Boat, speed", "60");
+    fleet->attributeIs("Truck, capacity", "50");
+    fleet->attributeIs("Plane, cost", "20");
+    cout << "fleet->attribute('Boat, speed'): " << fleet->attribute("Boat, speed") << endl;
+
+    // --- Create instances
+    // -- Locations
+    // customers
+    Ptr<Instance> customer1 = manager->instanceNew("customer1", "Customer");  
+    Ptr<Instance> customer2 = manager->instanceNew("customer2", "Customer");  
+    // ports
+    Ptr<Instance> port1 = manager->instanceNew("port1", "Port");  
+
+    if (!customer1 || !customer2 || !port1) {
+        cerr << "Unexpected NULL customer or port." << endl;
+	return 1;
+    }
+
+    // -- Segments
+    // boat
+    Ptr<Instance> boatSeg1 = manager->instanceNew("boatSeg1", "Boat segment");  
+    Ptr<Instance> boatSeg2 = manager->instanceNew("boatSeg2", "Boat segment");  
+    // truck
+    Ptr<Instance> truckSeg1 = manager->instanceNew("truckSeg1", "Truck segment");  
+    Ptr<Instance> truckSeg2 = manager->instanceNew("truckSeg2", "Truck segment");  
+
+    if (!boatSeg1 || !boatSeg2 || !truckSeg1 || !truckSeg2 ) {
+        cerr << "Unexpected NULL segment." << endl;
+        return 1;
+    }
+
+    // connections
+    // customer1 <---> port1
+    truckSeg1->attributeIs("source", "customer1");
+    truckSeg2->attributeIs("source", "port1");
+    truckSeg1->attributeIs("return segment", "truckSeg2");
+    cout << "truckSeg1->attribute('source'): " << truckSeg1->attribute("source") << endl;
+    
+    // customer2 <---> port1
+    boatSeg1->attributeIs("source", "customer2");
+    boatSeg2->attributeIs("source", "port1");
+    boatSeg1->attributeIs("return segment", "boatSeg2");
+    cout << "boatSeg1->attribute('return segment'): " << boatSeg1->attribute("return segment") << endl;
+
+    // -- Segment lengths
+    boatSeg1->attributeIs("length", "400");
+    boatSeg2->attributeIs("length", "400");
+    truckSeg1->attributeIs("length", "900");
+    truckSeg2->attributeIs("length", "900");
+
+    // -- Segment difficulties
+    boatSeg1->attributeIs("difficulty", "1");
+    boatSeg2->attributeIs("difficulty", "1");
+    truckSeg1->attributeIs("difficulty", "1");
+    truckSeg2->attributeIs("difficulty", "1");
+    
+    // -- Segment expedite support
+    boatSeg1->attributeIs("expedite support", "yes");
+    boatSeg2->attributeIs("expedite support", "yes");
+    truckSeg1->attributeIs("expedite support", "yes");
+    truckSeg2->attributeIs("expedite support", "yes");
+
+    // -- Connectivity queries
+    Ptr<Instance> conn = manager->instanceNew("myConn", "Conn");
+
+    if (!conn) {
+        cerr << "Unexpected NULL conn." << endl;
+        return 1;
+    }
+
+    cout << "**** explore customer1 : distance 1500 ****" << endl;
+    cout << conn->attribute("explore customer1 : distance 1500") << endl;
+    cout << endl;
+
+    cout << "*** connect customer2 : customer1 ****" << endl;
+    cout << conn->attribute("connect customer2 : customer1") << endl;
+    cout << endl;
+
+    // -- Statistics queries
+    cout << "===== Stats attributes =====" << endl;
+    cout << " --- Segments --- " << endl;
+    cout << "# Truck segments : " << stats->attribute("Truck segment") << endl;
+    cout << "Expediting %     : " << stats->attribute("expedite percentage") << endl;
+    cout << " --- Terminals --- " << endl;
+    cout << "# Plane terminals: " << stats->attribute("Plane terminal") << endl;
+    cout << " --- Ports and customers --- " << endl;
+    cout << "# Ports          : " << stats->attribute("Port") << endl;
+
+    cerr << "Done!" << endl;
+
+    return 0;
 }
 
 int example1() {
@@ -280,15 +396,69 @@ int example2() {
     // -- Segment lengths
     ts1->attributeIs("length", "400");
     ts1r->attributeIs("length", "400");
+    ts2->attributeIs("length", "600");
+    ts2r->attributeIs("length", "600");
+    ts3->attributeIs("length", "500");
+    ts3r->attributeIs("length", "500");
+    ts4->attributeIs("length", "100");
+    ts4r->attributeIs("length", "100");
+    ts5->attributeIs("length", "900");
+    ts5r->attributeIs("length", "900");
+	
+    bs1->attributeIs("length", "900");
+    bs1r->attributeIs("length", "900");
+    bs2->attributeIs("length", "200");
+    bs2r->attributeIs("length", "200");
+    bs3->attributeIs("length", "500");
+    bs3r->attributeIs("length", "500");
+    bs4->attributeIs("length", "100");
+    bs4r->attributeIs("length", "100");
+	
+    ps1->attributeIs("length", "100");
+    ps1r->attributeIs("length", "100");
+    ps2->attributeIs("length", "200");
+    ps2r->attributeIs("length", "200");
+    ps3->attributeIs("length", "300");
+    ps3r->attributeIs("length", "300");
+    ps4->attributeIs("length", "900");
+    ps4r->attributeIs("length", "900");
 
     // -- Segment difficulties
     ts1->attributeIs("difficulty", "1");
     ts1r->attributeIs("difficulty", "1");
+    ts2->attributeIs("difficulty", "1");
+    ts2r->attributeIs("difficulty", "1");
+    ts3->attributeIs("difficulty", "1");
+    ts3r->attributeIs("difficulty", "1");
+    ts4->attributeIs("difficulty", "1");
+    ts4r->attributeIs("difficulty", "1");
+    ts5->attributeIs("difficulty", "1");
+    ts5r->attributeIs("difficulty", "1");
+    bs1->attributeIs("difficulty", "1");
+    bs1r->attributeIs("difficulty", "1");
+    bs2->attributeIs("difficulty", "1");
+    bs2r->attributeIs("difficulty", "1");
+    bs3->attributeIs("difficulty", "1");
+    bs3r->attributeIs("difficulty", "1");
+    bs4->attributeIs("difficulty", "1");
+    bs4r->attributeIs("difficulty", "1");
+    ps1->attributeIs("difficulty", "1");
+    ps1r->attributeIs("difficulty", "1");
+    ps2->attributeIs("difficulty", "1");
+    ps2r->attributeIs("difficulty", "1");
+    ps3->attributeIs("difficulty", "1");
+    ps3r->attributeIs("difficulty", "1");
+    ps4->attributeIs("difficulty", "1");
+    ps4r->attributeIs("difficulty", "1");
     
     // -- Segment expedite support
 	ts1->attributeIs("expedite support", "yes");
+	ts4->attributeIs("expedite support", "yes");
+	ts3->attributeIs("expedite support", "yes");
+	bs4->attributeIs("expedite support", "yes");
 	
     cout << "ts1r->attribute('return segment'): " << ts1r->attribute("return segment") << endl;
+    cout << "ts1->attribute('expedite support'): " << ts1->attribute("expedite support") << endl;
     cout << "ts1r->attribute('expedite support'): " << ts1r->attribute("expedite support") << endl;
 
     // -- Connectivity queries
@@ -299,13 +469,16 @@ int example2() {
         return 1;
     }
 
-    cout << "**** explore customer1 : distance 1500 ****" << endl;
-    cout << conn->attribute("explore customer1 : distance 1500 cost 400 expedited time 10") << endl;
+	string q = "explore customer1 : distance 500";
+    cout << "**** "<< q << " ****" << endl;
+    cout << conn->attribute(q) << endl;
     cout << endl;
 
-    cout << "*** connect customer2 : customer1 ****" << endl;
-    cout << conn->attribute("connect customer2 : customer1") << endl;
+	q = "connect customer1 : port1";
+    cout << "**** "<< q << " ****" << endl;
+    cout << conn->attribute(q) << endl;
     cout << endl;
+
 
     // -- Statistics queries
     cout << "===== Stats attributes =====" << endl;
